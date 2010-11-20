01c8a480f0638e7654cc600e866eefbf4dd310b8
Andrew Nacin <andrewnacin@gmail.com>
2010-11-20 14:39:00 -0500

    Add plugin base.

---
+++ wp-npr-api.php
@@ -0,0 +1,20 @@
+<?php
+/*
+ * Plugin Name: NPR API
+ * Description: Woo.
+ * Version: 0.1-alpha
+ * Author: Mark Lavallee and Andrew Nacin
+ * License: GPLv2
+ */
+
+class NPR_API {
+	var $api_key = 'MDA2NDgxODUyMDEyOTAyODEzNjYxY2Q5Yw001';
+
+	function NPR_API {
+		
+	}
+
+
+
+}
+new NPR_API;
