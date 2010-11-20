c748638af7f87c824332cc2b6896681c239e7ce0
Marc Lavallee <mlavallee@gmail.com>
2010-11-20 14:41:44 -0500

    Name and API key scrub.

--- wp-npr-api.php
+++ wp-npr-api.php
@@ -3,12 +3,12 @@
  * Plugin Name: NPR API
  * Description: Woo.
  * Version: 0.1-alpha
- * Author: Mark Lavallee and Andrew Nacin
+ * Author: Marc Lavallee and Andrew Nacin
  * License: GPLv2
  */
 
 class NPR_API {
-	var $api_key = 'MDA2NDgxODUyMDEyOTAyODEzNjYxY2Q5Yw001';
+	var $api_key = '';
 
 	function NPR_API {
 		
